6f77528536a4ec13813f74587fc27146e081629e
LeahBasson <leahbasson54@gmail.com>
2025-03-05 00:12:22 +0200

    :tada:

---
+++ PerimeterAndAreaRectangle/src/PerimeterAndAreaRectangle.java
@@ -0,0 +1,16 @@
+
+public class PerimeterAndAreaRectangle {
+
+    
+    public static void main(String[] args) {
+        int length = 10;
+        int width = 20;
+        int perimeter = 2*(length + width);
+        int area = width*length;
+        
+        System.out.println("Perimeter: " + perimeter);
+        System.out.println("Area: " + area);
+        
+    }
+    
+}
